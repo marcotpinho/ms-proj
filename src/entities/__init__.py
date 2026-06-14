e8562b3e6ccbf2cdcbb73e92d0a0e91783b1d1cf
marcotpinho <marcot.pinho@gmail.com>
2026-06-14 16:54:33 -0300

    remove old file structure

--- src/entities/__init__.py
+++
@@ -1,9 +0,0 @@
-from .Solution import Solution
-from .DistanceModel import DistanceModel
-from .Neighborhood import Neighborhood
-from .Archive import Archive
-from .Runner import Runner
-from .Map import Map
-from .Evaluator import Evaluator
-
-__all__ = ["Solution", "Neighborhood", "DistanceModel", "Archive", "Runner", "Map", "Evaluator"]
--- src/operators/__init__.py
+++
@@ -1,4 +0,0 @@
-from .local_search import local_search
-from .perturb_solution import perturb_solution
-
-__all__ = ["local_search", "perturb_solution"]
